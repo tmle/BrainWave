439012364c4a7319d8bfd49801e7d8b299da1c81
Thinh M. Le <thinh.m.le@gmail.com>
2017-07-19 09:32:25 -0700

    Initial Commit

---
+++ BrainWave-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BrainWave-Swift
+//
+//  Created by Thinh Le on 2017-07-19.
+//  Copyright © 2017 Thinh Le. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
